fade1a9204de79e07636820763b57ade8182566d
Ol-create <oluola96@gmail.com>
2022-03-08 11:18:16 -0600

    Add style to toggle-menu

--- dom.js
+++ dom.js
@@ -3,5 +3,5 @@ let toggleMenu = document.getElementById("toggle-menu");
 function show(){ toggleMenu.style.display = "block"; }
 
 function hide() {
-    toggleMenu.style.display = 'none';
+    toggleMenu.style.display = "none";
 }
