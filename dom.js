b6af7d60a6aff90dd6e67049fd50efddc33d0867
Ol-create <oluola96@gmail.com>
2022-03-10 06:41:03 -0600

    Add eventlistener to close and menu buttons

---
+++ dom.js
@@ -0,0 +1,17 @@
+const toggleMenu = document.getElementById('toggle-menu');
+
+function show(){ 
+    toggleMenu.style.display = 'block'; 
+}
+
+function hide() {
+    toggleMenu.style.display = 'none';
+}
+
+document.getElementById('openMenu').addEventListener('click', () => {
+  show();
+});
+
+document.getElementById('close').addEventListener('click', () => {
+  hide();
+});
