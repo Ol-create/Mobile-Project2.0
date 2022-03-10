25c91a6a94989f1bdbe3326414a5a42af9d44f95
Ol-create <oluola96@gmail.com>
2022-03-10 16:53:13 -0600

    Resolve reviewer comment

--- dom.js
+++ dom.js
@@ -8,10 +8,18 @@ function hide() {
   toggleMenu.style.display = 'none';
 }
 
+function openNav() {
+  toggleMenu.style.width = '100%';
+}
+
+function closeNav() {
+  toggleMenu.style.width = '0%';
+}
+
 document.getElementById('openMenu').addEventListener('click', () => {
-  show();
+  openNav();
 });
 
 document.getElementById('close').addEventListener('click', () => {
-  hide();
+  closeNav();
 });
