2b655d39c822515e4c72f2b39b0599b10d143a31
Ol-create <oluola96@gmail.com>
2022-03-10 06:10:32 -0600

    Add node for error text message

--- dom.js
+++
@@ -1,9 +0,0 @@
-const toggleMenu = document.getElementById('toggle-menu');
-
-function show(){ 
-    toggleMenu.style.display = 'block'; 
-}
-
-function hide() {
-    toggleMenu.style.display = 'none';
-}
