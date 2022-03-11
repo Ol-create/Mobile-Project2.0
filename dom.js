f049f9c7f40c24ab6a55dffebdb9b2e4129f348e
Ol-create <oluola96@gmail.com>
2022-03-11 06:01:24 -0600

    update with create-mobile-menu... dom.js

--- dom.js
+++ dom.js
@@ -1,12 +1,29 @@
 const toggleMenu = document.getElementById('toggle-menu');
 
-function show(){ 
-    toggleMenu.style.display = 'block'; 
+function hide() {
+  toggleMenu.style.display = 'none';
 }
 
-function hide() {
-    toggleMenu.style.display = 'none';
+document.querySelectorAll('.mylinks').forEach((n) => n.addEventListener('click', () => {
+  hide();
+}));
+
+function openNav() {
+  toggleMenu.style.display = 'block';
 }
+
+function closeNav() {
+  toggleMenu.style.display = 'none';
+}
+
+document.getElementById('openMenu').addEventListener('click', () => {
+  openNav();
+});
+
+document.getElementById('close').addEventListener('click', () => {
+  closeNav();
+});
+
 
 // Form Validation
 
