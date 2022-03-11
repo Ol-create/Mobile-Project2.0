032770e9fd7788189ed98117f6dff06f219ab442
Abraha Kahsay <abrahak81@gmail.com>
2022-03-11 03:53:05 +0300

    Create variable for form, email, error message

--- dom.js
+++ dom.js
@@ -8,3 +8,9 @@ function hide() {
     toggleMenu.style.display = 'none';
 }
 
+// Form Validation
+
+//Create variable for form, email, error message. 
+const form = document.querySelector('#form'); 
+const idEmail = form.email; 
+const errorMessage = document.getElementById('invalidText'); 
